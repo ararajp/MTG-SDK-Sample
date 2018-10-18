7a404c0aad53fa896eba36e9566d422052124d5a
Yusuke Arai <aralabjp@gmail.com>
2018-10-19 01:04:07 +0900

    add project

---
+++ MTG-SDK-Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MTG-SDK-Sample
+//
+//  Created by Yusuke Arai on 2018/10/19.
+//  Copyright © 2018年 Yusuke Arai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MTG-SDK-SampleTests/MTG_SDK_SampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  MTG_SDK_SampleTests.swift
+//  MTG-SDK-SampleTests
+//
+//  Created by Yusuke Arai on 2018/10/19.
+//  Copyright © 2018年 Yusuke Arai. All rights reserved.
+//
+
+import XCTest
+@testable import MTG_SDK_Sample
+
+class MTG_SDK_SampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
